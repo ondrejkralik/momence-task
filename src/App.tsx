87375f5dba1636f262ab31dda4fc07fbd4642ca4
Ondrej Kralik <ondrej.kralik@gmail.com>
2023-03-24 23:09:13 +0100

    feat: Show raw data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+
+const API_URL =
+  'https://api.allorigins.win/raw?url=https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt';
+
 const App: React.FC = () => {
-  return <div>Momence</div>;
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => fetch(API_URL).then(res => res.text()),
+  });
+
+  return (
+    <div>
+      <h1>Exchange rates</h1>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : error ? (
+        <span>Error: {error.toString()}</span>
+      ) : (
+        <pre>{data}</pre>
+      )}
+    </div>
+  );
 };
 
 export default App;
